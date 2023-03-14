97bd3bd917ecc6a1a5ca8c28f004807d8bbdd681
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-03-15 03:47:43 +0530

    changes done

--- anna-shakti/src/component/AboutPage.js
+++ anna-shakti/src/component/AboutPage.js
@@ -2,13 +2,54 @@ import React from 'react';
 
 
 function AboutPage() {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <p>Anna Shakti is a non-profit organization dedicated to fighting hunger and food waste. Our mission is to distribute surplus food to those in need while promoting sustainable practices and raising awareness about food waste.</p>
-      <p>Founded in 2023, Anna Shakti has since distributed over 1 million meals to those in need across India. We work with local businesses, farmers, and food banks to collect and distribute surplus food that would otherwise go to waste.</p>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        h1 > About Us < /h1> <
+        p > Anna Shakti is a non - profit organization dedicated to fighting hunger and food waste by distributing surplus food to those in need.Our mission is to promote sustainable practices and raise awareness about food waste
+        while providing nutritious meals to those who need it most. < /p> <
+        p > Our new food donation website makes it easy
+        for businesses, farmers, and individuals to donate their surplus food directly to those in need.With just a few clicks, you can make a difference in your community and help reduce food waste. < /p> <
+        p > The app is available
+        for free on both iOS and Android devices, and allows users to: < /p> <
+        ul >
+        <
+        li > Donate surplus food directly to local organizations < /li> <
+        li > Find nearby donation centers and food banks < /li> <
+        li > Track their donations and receive tax receipts < /li> <
+        li > Connect with other like - minded individuals and businesses < /li> <
+        /ul> <
+        p > Join the Anna Shakti community and help us make a difference today! < /p> <
+        p > User - Friendly Design: Our website is designed with the user in mind.We 've created an intuitive, easy-to-use interface that makes it simple for users to find the information they need, navigate the site, and make donations.</p> <
+        li > Secure Donations: We understand that security is a top concern when it comes to online donations.That 's why we'
+        ve implemented the latest security measures to ensure that all donations made through our website are safe and secure. < /li> <
+        li > Mobile Responsive: Our website is optimized
+        for mobile devices, so users can access it from anywhere, at any time.Whether they 're on their desktop computer, tablet, or smartphone, our website will look great and be easy to use.</li> <
+        li > Informational Resources: We want to provide our users with all the information they need to make informed decisions about their donations.That 's why we'
+        ve created a range of informational resources, including blog posts, FAQs, and guides, to help users understand the impact of their donations and how they can get involved. < /li> <
+        li > Volunteer Opportunities: Our website also provides information on volunteer opportunities, allowing users to get involved in their local community and support our mission in new and meaningful ways.With our food donation app, we 've made it easier than ever for individuals, businesses, and farmers to donate their surplus food to local organizations and food banks.</li> <
+        p > We believe that everyone can make a difference in the fight against food waste and hunger, and our app is designed to help you do just that. < /p>
+
+        <
+        h2 > Our Impact < /h2> <
+        p > Since our founding in 2023, Anna Shakti has distributed over 1 million meals to those in need across India.Our food donation app has made it even easier
+        for us to connect donors with local organizations and reduce food waste in our communities. < /p> <
+        p > By using our app, you 're not only making a difference in the fight against hunger and food waste, but you'
+        re also helping to build a more sustainable and equitable food system
+        for all.Thank you
+        for your support! < /p> <
+        li > Reduced Food Waste: By connecting food donors with food banks and other charitable organizations, our app helps to reduce food waste.By redirecting surplus food to those in need, we 're able to reduce the amount of food that goes to waste and help protect the environment.</li>
+
+        <
+        li > Improved Food Security: Our website helps to improve food security
+        for those in need.By providing access to surplus food, we 're able to ensure that more people have access to the nutritious food they need to lead healthy, active lives.</li> <
+        li > Stronger Communities: Our website helps to build stronger communities by bringing people together around a common cause.By working together to fight hunger and food waste, we 're able to build a more compassionate and connected society.</li>               <
+        li > Reduced Costs: Our website helps to reduce the costs associated with food waste.By redirecting surplus food to those in need, we 're able to reduce the costs of disposing of that food and help businesses save money on waste management.</li> <
+        li > Healthier Planet: By reducing food waste, our website helps to protect the planet.Food waste is a major contributor to greenhouse gas emissions, and by reducing the amount of food that goes to waste, we 're able to help mitigate the impacts of climate change and build a more sustainable future.</li>
+
+        <
+        /div>
+    );
 }
 
 export default AboutPage;
--- anna-shakti/src/component/WhatWeDoPage.js
+++ anna-shakti/src/component/WhatWeDoPage.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 function WhatWeDoPage() {
-  return (
-    <div>
-      <h1>What We Do</h1>
-      <p>We provide food and shelter to those in need.</p>
-      <p>Our mission is to ensure that no one goes hungry or without a roof over their head.</p>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        h1 > What We Do.. ? < /h1> <
+        p > We provide food and shelter to those in need. < /p> <
+        p > Our mission is to ensure that no one goes hungry or without a roof over their head. < /p> <
+        li > Reduced Food Waste : By connecting food donors with food banks and other charitable organizations, our app helps to reduce food waste.By redirecting surplus food to those in need, we 're able to reduce the amount of food that goes to waste and help protect the environment.</li> <
+        li > Improved Food Security: Our app helps to improve food security
+        for those in need.By providing access to surplus food, we 're able to ensure that more people have access to the nutritious food they need to lead healthy, active lives.</li> <
+        li > Stronger Communities: Our app helps to build stronger communities by bringing people together around a common cause.By working together to fight hunger and food waste, we 're able to build a more compassionate and connected society.</li> <
+        li > Reduced Costs: Our app helps to reduce the costs associated with food waste.By redirecting surplus food to those in need, we 're able to reduce the costs of disposing of that food and help businesses save money on waste management.</li> <
+        li > Healthier Planet: By reducing food waste, our app helps to protect the planet.Food waste is a major contributor to greenhouse gas emissions, and by reducing the amount of food that goes to waste, we 're able to help mitigate the impacts of climate change and build a more sustainable future.</li> <
+        li > Food Collection: Our app allows users to easily connect with local businesses, farms, and other organizations to collect surplus food that would otherwise go to waste.We work with a network of partners to collect and transport this food to where it 's needed most.</li> <
+        li > Food Distribution: Once food is collected, our app helps to distribute it to those in need.We work with local food banks and other charitable organizations to ensure that surplus food is distributed fairly and efficiently. < /li> <
+        li > Volunteer Engagement: Our app provides opportunities
+        for volunteers to get involved in our efforts to fight hunger and food waste.Volunteers can help with food collection, distribution, and other activities, and we provide training and support to ensure that everyone has a positive and meaningful experience. < /li> <
+        li > Public Awareness: Our app helps to raise awareness about food waste and hunger in our communities.We use social media, events, and other outreach efforts to educate the public about the impact of food waste and the importance of food donation. < /li> <
+        li > Data Analysis: Our app collects data on food waste and distribution, which we use to optimize our operations and improve our impact.By analyzing this data, we can identify trends and opportunities
+        for improvement, and ensure that surplus food is distributed as effectively as possible. < /li> <
+        /div>
+    );
 }
 
 export default WhatWeDoPage;
