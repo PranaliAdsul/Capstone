7f64f644c405fd4f159ea62852dd365c7f70c8d7
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-03-15 15:59:00 +0530

    changes

--- anna-shakti/src/component/HomePage.js
+++ anna-shakti/src/component/HomePage.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 function HomePage() {
-    return ( <
-        div >
-        <
-        h1 > Welcome to Anna Shakti < /h1> <
-        p > We are a non - profit organization dedicated to ending hunger and poverty in our community. < /p> <
-        /div>
-    );
+    return ( <div >
+        <h1 > Welcome to Anna Shakti </h1> 
+        <p> We are a non - profit organization dedicated to ending hunger and poverty in our community. </p> 
+        </div>
+    )
 }
-
 export default HomePage;
